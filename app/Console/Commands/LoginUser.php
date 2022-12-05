4f3ec7368abd128cf50030ad7c4f609364928347
vandelshtam <mvlju977@gmail.com>
2022-12-05 21:51:03 +0300

    task_1 part_1

---
+++ app/Console/Commands/LoginUser.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Response\GeneralResponse;
+use App\Http\Controllers\UserController;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Contracts\Encryption\DecryptException;
+
+
+class LoginUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'login:user {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Login user';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->argument("email");
+        $password = $this->argument("password");
+
+        // if(!User::where("email", $email)->count()){
+        //     $this->info("Email not faund");
+        // }
+
+        // //$credentials = $this->argument(["email", "password"]);
+
+        // if (!$token = auth()->attempt([$email,$password])) {
+        //     $this->info("User Unauthorized");
+        // }
+        // try {
+        //     $user = auth()->userOrFail();
+        // } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+        //     $this->info("User not faund");
+        // }
+
+        // // Get the token
+        // //$token = auth()->login($user);
+        // $auth_factory = auth()->factory()->getTTL() * 10;
+
+        // $this->info("access_token :".$token);
+        // $this->info("experies_is : ".$auth_factory);
+        // $this->info( "token_type :"." bearer");
+        // $this->info( $password);
+
+        //Get some user from somewhere
+        if(!User::where("email", $email)->count()){
+            $this->info("User not faund");
+        }
+        elseif($user = User::firstOrNew(['email' => $email],['password' => $password])){
+            // Get the token
+            if (Hash::check($password, $user['password'])) {
+                $token = auth()->login($user);
+                $auth_factory = auth()->factory()->getTTL() * 10;
+
+                $this->info("access_token :".$token);
+                $this->info("experies_is : ".$auth_factory);
+                $this->info( "token_type :"." bearer");
+            }
+            else{
+            $this->info("Password not faund");
+            }
+        }
+           
+    }
+
+}
